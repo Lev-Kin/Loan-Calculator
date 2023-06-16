f391ed2ebb5fb27f4f88fdf1d76b4926e933503b
Lev <lev@lev>
2023-06-16 09:28:57 +0300

    3 of 4 stages completed

--- Loan Calculator/task/creditcalc/creditcalc.py
+++ Loan Calculator/task/creditcalc/creditcalc.py
@@ -1,11 +1,48 @@
-loan_principal = 'Loan principal: 1000'
-final_output = 'The loan has been repaid!'
-first_month = 'Month 1: repaid 250'
-second_month = 'Month 2: repaid 250'
-third_month = 'Month 3: repaid 500'
+import math
 
-print(loan_principal)
-print(first_month)
-print(second_month)
-print(third_month)
-print(final_output)
+def calculate_monthly_payments(principal, payment, interest):
+    interest = interest / 100 / 12
+    months = math.log(payment / (payment - interest * principal), 1 + interest)
+    return math.ceil(months)
+
+def calculate_monthly_payment(principal, periods, interest):
+    interest = interest / 100 / 12
+    payment = principal * (interest * pow(1 + interest, periods)) / (pow(1 + interest, periods) - 1)
+    return math.ceil(payment)
+
+def calculate_loan_principal(payment, periods, interest):
+    interest = interest / 100 / 12
+    principal = payment / ((interest * pow(1 + interest, periods)) / (pow(1 + interest, periods) - 1))
+    return principal
+
+calculation_type = input("What do you want to calculate?\n"
+                         'type "n" for number of monthly payments,\n'
+                         'type "a" for annuity monthly payment amount,\n'
+                         'type "p" for loan principal:\n')
+
+if calculation_type == 'n':
+    principal = float(input("Enter the loan principal:\n"))
+    payment = float(input("Enter the monthly payment:\n"))
+    interest = float(input("Enter the loan interest:\n"))
+    months = calculate_monthly_payments(principal, payment, interest)
+    years, months = divmod(months, 12)
+    if years == 0:
+        print(f"It will take {months} months to repay this loan!")
+    elif months == 0:
+        print(f"It will take {years} years to repay this loan!")
+    else:
+        print(f"It will take {years} years and {months} months to repay this loan!")
+
+elif calculation_type == 'a':
+    principal = float(input("Enter the loan principal:\n"))
+    periods = int(input("Enter the number of periods:\n"))
+    interest = float(input("Enter the loan interest:\n"))
+    payment = calculate_monthly_payment(principal, periods, interest)
+    print(f"Your monthly payment = {payment}!")
+
+elif calculation_type == 'p':
+    payment = float(input("Enter the annuity payment:\n"))
+    periods = int(input("Enter the number of periods:\n"))
+    interest = float(input("Enter the loan interest:\n"))
+    principal = calculate_loan_principal(payment, periods, interest)
+    print(f"Your loan principal = {principal}!")
